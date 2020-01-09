545ce06722d6f116d1e639735f376b8ef36610fb
tanyatroyanova <tanushatroyanova1998@gmail.com>
2020-01-10 00:30:59 +0300

    create js variables

---
+++ 1.js
@@ -0,0 +1,9 @@
+// 1. Объявить переменную, вывести в консоли 
+
+let str = "Hello!";
+console.log(str);
+
+// 2. Объявить константу, вывести в консоли 
+
+let STR = "Hello Tanya!";
+console.log(STR);
