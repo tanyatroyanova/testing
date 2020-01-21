419a3ab95e489feaa325c607a989a9b028984d8b
tanyatroyanova <tanushatroyanova1998@gmail.com>
2020-01-21 23:00:35 +0300

    исправила недочеты в коде из комментариев

--- switch.js
+++ switch.js
@@ -1,5 +1,5 @@
-//#1
-/*Напишите if..else, соответствующий следующему switch:
+// #1
+/* Напишите if..else, соответствующий следующему switch:
 
 switch (browser) {
   case 'Edge':
@@ -18,16 +18,16 @@ switch (browser) {
 }*/
 
 let browser;
-if (browser === 'Edge'){
-    alert( "You've got the Edge!" );
-}else if(browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera'){
-    alert( 'Okay we support these browsers too' );
-}else {
-    alert( 'We hope that this page looks ok!' );
+if (browser === 'Edge') {
+    alert("You've got the Edge!");
+} else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+    alert('Okay we support these browsers too');
+} else {
+    alert('We hope that this page looks ok!');
 }
 
-//#2
-/*Перепишите код с использованием одной конструкции switch:
+// #2
+/* Перепишите код с использованием одной конструкции switch:
 
 const number = +prompt('Введите число между 0 и 3', '');
 
