13c0d92505fdf626e4d707c5256448c8570bb8cc
Tatsiana Trayanava <tanushatroyanova1998@gmail.com>
2020-02-02 23:19:43 +0300

    добавила задания по теме прототипное наследование

--- inheritance.js
+++ inheritance.js
@@ -111,3 +111,107 @@ let rabbit = new Rabbit("Кроль");
 alert( rabbit.hasOwnProperty('name') ); // true
 
 // не работает т.к. необходимо вызывать конструктор родительского класса
+
+/////////////////////////////////Прототипное наследование/////////////////////////////////////////////
+// #1
+// В приведённом ниже коде создаются и изменяются два объекта.
+// Какие значения показываются в процессе выполнения кода?
+
+let animal = {
+  jumps: null
+};
+let rabbit = {
+  __proto__: animal,
+  jumps: true
+};
+
+alert( rabbit.jumps ); // true (1)
+
+delete rabbit.jumps;
+
+alert( rabbit.jumps ); // null (2)
+
+delete animal.jumps;
+
+alert( rabbit.jumps ); // undefined (3)
+//Должно быть 3 ответа.
+
+// #2
+// Задача состоит из двух частей.
+
+// У нас есть объекты:
+
+let head = {
+  glasses: 1
+};
+
+let table = {
+  pen: 3
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2
+};
+
+let pockets = {
+  money: 2000
+};
+/* С помощью свойства __proto__ задайте прототипы так, чтобы поиск любого свойства выполнялся по следующему пути: 
+pockets → bed → table → head. Например, pockets.pen должно возвращать значение 3 (найденное в table), а bed.glasses – значение 1 (найденное в head).
+Ответьте на вопрос: как быстрее получить значение glasses – через pockets.glasses или через head.glasses? 
+При необходимости составьте цепочки поиска и сравните их.*/
+
+pockets.__proto__ = bed;
+bed.__proto__ = table;
+table.__proto__ = head;
+
+// как быстрее получить значение glasses – через pockets.glasses или через head.glasses?
+// Я думаю, что быстрее будет через head.glasses, т.к. данное св-во содержится именно в этом объекте
+// чтобы получить значение glasses через pockets.glasses, будет происходить поиск этого св-ва по цепочке от объкта  pockets до head.
+
+// #3
+// Объект rabbit наследует от объекта animal.
+
+//Какой объект получит свойство full при вызове rabbit.eat(): animal или rabbit?
+
+let animal = {
+  eat() {
+    this.full = true;
+  }
+};
+
+let rabbit = {
+  __proto__: animal
+};
+
+rabbit.eat();
+// Ответ: свойство full при вызове rabbit.eat() полчуит объект rabbit т.к. this указывает на используемый объект при вызове
+
+// #4
+// У нас есть два хомяка: шустрый (speedy) и ленивый (lazy); оба наследуют от общего объекта hamster.
+
+//  Когда мы кормим одного хомяка, второй тоже наедается. Почему? Как это исправить?
+
+let hamster = {
+  stomach: [],
+
+  eat(food) {
+    this.stomach = [food];
+  }
+};
+
+let speedy = {
+  __proto__: hamster
+};
+
+let lazy = {
+  __proto__: hamster
+};
+
+// Этот хомяк нашёл еду
+speedy.eat("apple");
+alert( speedy.stomach ); // apple
+
+// У этого хомяка тоже есть еда. Почему? Исправьте
+alert( lazy.stomach ); // apple
