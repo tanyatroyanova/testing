5d33e3c43caef7aa34c9e01939cca52bfe9bf22b
tanyatroyanova <tanushatroyanova1998@gmail.com>
2020-03-02 17:38:49 +0300

    исправила assertions

--- protractor/conf/conf.js
+++ protractor/conf/conf.js
@@ -12,7 +12,7 @@ exports.config = {
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['../tests/first_test.js'],
+  specs: ['../tests/*.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
--- protractor/tests/assertions.js
+++ protractor/tests/assertions.js
@@ -2,22 +2,22 @@ describe("Assertions tests", function() {
     it("сравнение двух чисел", function() {
       let a = 1;
       let b = 1;
-      expeсt(a).toBe(b);
+      expect(a).toBe(b);
       expect(a).toEqual(b);      
     });
   
     it("сравнение двух строк на полное совпадение", function() {
-      expeсt('abc').toMatch('abc');     
+      expect('abc').toEqual('abc');     
     });
   
-    it("сравнение двух строк на частичное совпадени(содержит)", function() {
-      expeсt('aaaabcrefgweg').toContain('abc');     
+    it("сравнение двух строк на частичное совпадение(содержит)", function() {
+      expect('aaaabcrefgweg').toContain('abc');     
     });
   
     it("сравнение массивов", function() {
       let arr1 = [1, 2, 3];
       let arr2 = [1, 2, 3];
-      expeсt(arr1).toEqual(arr2);   
+      expect(arr1).toEqual(arr2);   
     });
   });
   
