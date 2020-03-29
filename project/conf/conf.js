545c1025df5aedca3f395a167c8fdb6b06ba09b3
tanyatroyanova <tanushatroyanova1998@gmail.com>
2020-03-30 00:09:15 +0300

    add changes in gitignore and config files

--- project/conf/conf.js
+++ project/conf/conf.js
@@ -20,5 +20,46 @@ exports.config = {
     defaultTimeoutInterval: 60000
   },
 
-  restartBrowserBetweenTests: true
+  restartBrowserBetweenTests: true,
+
+  onPrepare: function() {
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+
+    var jasmineReporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+        consolidateAll: true,
+        savePath: './',
+        filePrefix: 'xmlresults'
+    }));
+  },
+  
+  //HTMLReport called once tests are finished
+  onComplete: function() {
+    var browserName, browserVersion;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+      browserName = caps.get('browserName');
+      browserVersion = caps.get('version');
+      platform = caps.get('platform');
+
+      var HTMLReport = require('protractor-html-reporter-2');
+
+      testConfig = {
+          reportTitle: 'Protractor Test Execution Report',
+          outputPath: './',
+          outputFilename: 'ProtractorTestReport',
+          screenshotPath: './screenshots',
+          testBrowser: browserName,
+          browserVersion: browserVersion,
+          modifiedSuiteName: false,
+          screenshotsOnlyOnFailure: true,
+          testPlatform: platform
+      };
+      new HTMLReport().from('xmlresults.xml', testConfig);
+  });
+  }
 };
